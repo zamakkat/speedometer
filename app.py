a07929ecd9782928c98c63f3d77fca7ee71ba4e9
nglinh <nglinh2312@gmail.com>
2015-01-25 01:22:42 +0800

    changed name for understandability

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def clone_new_repo():
         return 'cannot deploy the new project. Test your deployment first dude!', 403
     return 'done!', 200
 
-@app.route('/pull/<repo_name>')
-def fetch_repo(repo_name):
-    if os.system('cd ~/external/%s' %(repo_name)) != 0:
+@app.route('/pull/<project_name>')
+def fetch_repo(project_name):
+    if os.system('cd ~/external/%s' %(project_name)) != 0:
         return 'repo not existed or cannot be accessed, try using /clone instead', 403
-    elif os.system('cd ~/external/%s && git pull origin master' %s(repo_name)) != 0:
+    elif os.system('cd ~/external/%s && git pull origin master' %s(project_name)) != 0:
         return 'repo cannot be pulled. Please check repo setting', 403
-    elif os.system('cd ~/external/%s && git push boostiodokku master' %s(repo_name)) != 0:
+    elif os.system('cd ~/external/%s && git push boostiodokku master' %s(project_name)) != 0:
         return 'cannot deploy the new project. Test your deployment first dude!', 403
     return 'repo updated', 200
 
