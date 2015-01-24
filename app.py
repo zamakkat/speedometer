937437c54c0ff9daa6a9108826484c54e34716ac
Duong Ho Tuan <zamakkat@gmail.com>
2015-01-24 21:13:38 +0800

    Added testing framework

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
+import shutil
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -7,6 +9,14 @@ client = MongoClient('localhost', 27017)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/run')
+def run():
+    project_name = "project1"
+    shutil.copytree("test", project_name)
+    os.system("multimech-run " + project_name)
+    return 'OK!'
+
 
 if __name__ == '__main__':
     app.run()
---
+++ tests/sample/test_scripts/windows_chrome.py
@@ -0,0 +1,30 @@
+import mechanize
+import time
+
+class Transaction(object):
+    # def __init__(self):
+        # do per-user user setup here
+        # this gets called once on user creation
+        # pass
+
+    def run(self):
+        # create a Browser instance
+        br = mechanize.Browser()
+        # don't bother with robots.txt
+        br.set_handle_robots(False)
+        # add a custom header so wikipedia allows our requests
+        br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36')]
+
+        # start the timer
+        start_timer = time.time()
+        # submit the request
+        resp = br.open('http://www.wikipedia.org/')
+        resp.read()
+        # stop the timer
+        latency = time.time() - start_timer
+
+        # store the custom timer
+        self.custom_timers['Load_Front_Page'] = latency
+
+        # verify responses are valid
+        assert (resp.code == 200), 'Bad Response: HTTP %s' % resp.code
